cc02b5ba7ac87cada2a6ac8c5cb5387b332ba713
Kasualix <670067575@qq.com>
2023-09-26 19:10:06 +0800

    compatibility with new version of the mod

--- src/main/java/com/teampotato/safefall/mixin/MixinStarfallSpell.java
+++ src/main/java/com/teampotato/safefall/mixin/MixinStarfallSpell.java
@@ -7,24 +7,25 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = StarfallSpell.class, remap = false)
 public abstract class MixinStarfallSpell {
-    @Shadow protected abstract float getDamage(LivingEntity caster);
+    @Shadow protected abstract float getDamage(int spellLevel, LivingEntity caster);
 
     /**
      * @author Kasualix
      * @reason safefall
      */
     @Overwrite
-    public void shootComet(Level world, LivingEntity entity, Vec3 spawn) {
+    public void shootComet(Level world, int spellLevel, LivingEntity entity, @NotNull Vec3 spawn) {
         Comet fireball = new Comet(world, entity);
         fireball.setPos(spawn.add(-1.0, 0.0, 0.0));
         fireball.shoot(new Vec3(0.15000000596046448, -0.8500000238418579, 0.0), 0.075F);
-        fireball.setDamage(this.getDamage(entity));
+        fireball.setDamage(this.getDamage(spellLevel, entity));
         fireball.setExplosionRadius(2.0F);
         fireball.addTag("safefall");
         world.addFreshEntity(fireball);
