edf02abda87e75314497e9d86a3605e3583afb8f
Kasualix <670067575@qq.com>
2023-10-02 22:48:39 +0800

    1201

--- src/main/java/com/teampotato/safefall/mixin/MixinPlayer.java
+++ src/main/java/com/teampotato/safefall/mixin/MixinPlayer.java
@@ -3,6 +3,7 @@ package com.teampotato.safefall.mixin;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Player.class)
 public abstract class MixinPlayer {
     @Inject(method = "hurt", at = @At("HEAD"), cancellable = true)
-    private void onHurt(DamageSource pSource, float pAmount, CallbackInfoReturnable<Boolean> cir) {
+    private void onHurt(@NotNull DamageSource pSource, float pAmount, CallbackInfoReturnable<Boolean> cir) {
         Entity sourceEntity = pSource.getEntity();
         Entity directSourceEntity = pSource.getDirectEntity();
         if ((sourceEntity != null && sourceEntity.getTags().contains("safefall")) || (directSourceEntity != null && directSourceEntity.getTags().contains("safefall"))) {
--- src/main/java/com/teampotato/safefall/mixin/MixinStarfallSpell.java
+++ src/main/java/com/teampotato/safefall/mixin/MixinStarfallSpell.java
@@ -5,6 +5,7 @@ import io.redspace.ironsspellbooks.spells.ender.StarfallSpell;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
@@ -29,6 +30,6 @@ public abstract class MixinStarfallSpell {
         fireball.setExplosionRadius(2.0F);
         fireball.addTag("safefall");
         world.addFreshEntity(fireball);
-        world.playSound(null, spawn.x, spawn.y, spawn.z, SoundEvents.FIREWORK_ROCKET_LAUNCH, SoundSource.PLAYERS, 3.0F, 0.7F + world.random.nextFloat() * 0.3F);
+        world.playSound((Player)null, spawn.x, spawn.y, spawn.z, SoundEvents.FIREWORK_ROCKET_LAUNCH, SoundSource.PLAYERS, 3.0F, 0.7F + world.random.nextFloat() * 0.3F);
     }
 }
